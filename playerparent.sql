91737861445031b16f96abc1c77c75d66a1e53e1
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-11-17 13:21:49 +0200

    Update playerparent.sql

--- playerparent.sql
+++ playerparent.sql
@@ -1,6 +1,6 @@
 SELECT Parent.id, Player.name, Parent.lastName FROM Player LEFT JOIN Parent ON Player.id = Parent.id
 --------------------------------
-SELECT Player.name, Player.lastName , Player.id 
+SELECT Player.name, Player.last_name , Parent.firstName, Parent.lastName 
 FROM Player 
-LEFT JOIN PlayerParent ON Player.id = PlayerParent.parent_id 
-LEFT JOIN Parent ON Player.name = Parent.firstName
+LEFT JOIN PlayerParent ON Player.id = PlayerParent.player_id 
+LEFT JOIN Parent ON Parent.id = PlayerParent.parent_id
